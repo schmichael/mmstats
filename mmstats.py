a7d010691d15c3fd985b7a19db3f0a50461017d2
Michael Schurter <michael@susens-schurter.com>
2011-05-30 17:35:36 -0700

    First attempt at memory mapped stats. Painfully ugly and incomplete

---
+++ mmstats.py
@@ -0,0 +1,112 @@
+import ctypes
+import mmap
+import os
+import struct
+import tempfile
+import threading
+
+
+BUFFER_IDX_TYPE = ctypes.c_byte
+LABEL_SZ_TYPE = ctypes.c_ushort
+
+
+def _init_mmap(path=None, filename=None, size=None):
+    """Create mmap instance"""
+    if path is None:
+        path = tempfile.gettempdir()
+
+    if filename is None:
+        filename = 'mmstats-%d' % os.getpid()
+        t = threading.current_thread()
+        if t.ident:
+            filename += '-%d' % t.ident
+
+    full_path = os.path.join(path, filename)
+
+    # Create new empty file to back memory map on disk
+    fd = os.open(full_path, os.O_CREAT | os.O_TRUNC | os.O_RDWR)
+
+    # Zero out the file to insure it's the right size
+    os.write(fd, '\x00' * mmap.PAGESIZE)
+
+    return mmap.mmap(fd, mmap.PAGESIZE, mmap.MAP_SHARED, mmap.PROT_WRITE)
+
+
+def _struct_factory(label_sz, buffer_type):
+    """Helper function to generate the proper stat field structure"""
+
+    #FIXME There's got to be a more elegant way. There are 3 dynamic
+    #      fields: label & the 2 value buffers
+    class _Struct(ctypes.Structure):
+        _fields_ = [
+            ('label_sz', ctypes.c_ushort),
+            ('label', (ctypes.c_char * label_sz)),
+            ('write', ctypes.c_byte),
+            ('a', buffer_type),
+            ('b', buffer_type)
+        ]
+
+    return _Struct
+
+
+class Stat(object):
+    """Sentinel class"""
+
+
+class UIntStat(Stat):
+    """32bit Unsigned Integer field"""
+
+    def _init(self, label, mm, offset):
+        if isinstance(label, unicode):
+           label = label.encode('utf8')
+
+        _Struct = _struct_factory(len(label), ctypes.c_uint)
+
+        self._struct = _Struct.from_buffer(mm, offset)
+        self._struct.label_sz = len(label)
+        self._struct.label = label
+        self._struct.write = 0
+        self._struct.a = 0
+        self._struct.b = 0
+        #self.buffers = (self._struct.a, self._struct.b)
+        return offset + ctypes.sizeof(_Struct)
+
+    # TODO Support descriptor protocol
+    def get(self):
+        # Get from the read buffer
+        #return self.buffers[self._struct.write.value ^ 1].value
+        if self._struct.write:
+            return self._struct.a
+        else:
+            return self._struct.b
+
+    def set(self, val):
+        # Set the write buffer
+        if self._struct.write:
+            self._struct.b = val
+        else:
+            self._struct.a = val
+        self._struct.write ^= 1
+
+        #self.buffers[self._struct.write] = val
+        # Swap the write buffer
+        #self._struct.write ^= 1
+
+
+class MmStats(object):
+
+    def __init__(self):
+        mmap_ = _init_mmap()
+        mmap_[0] = '\x01' # Stupid version number
+        offset = 1
+
+        for attrname in dir(self):
+            print 'Found %s' % attrname
+            attr = getattr(self, attrname)
+            if isinstance(attr, Stat):
+                print '%s is a stat' % attrname
+                offset = attr._init(attrname, mmap_, offset)
+                print 'New offset %d' % offset
+
+        self.mmap = mmap_
+        self.offset = offset
