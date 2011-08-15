f2752572d915563ea5a3361dbb7a3fee08b04660
Michael Schurter <michael@susens-schurter.com>
2011-08-14 23:47:19 -0700

    Make basic test a bit more thorough

--- tests/test_mmstats.py
+++ tests/test_mmstats.py
@@ -2,6 +2,7 @@ import mmstats
 
 def test_uint():
     class MyStats(mmstats.MmStats):
+        zebras = mmstats.UIntStat()
         apples = mmstats.UIntStat()
         oranges = mmstats.UIntStat()
 
@@ -11,11 +12,19 @@ def test_uint():
     assert mmst.mmap[0] == '\x01'
     assert mmst.mmap.find('applesL') != -1
     assert mmst.mmap.find('orangesL') != -1
+    assert mmst.mmap.find('zebrasL') != -1
 
     # Stat manipulation
     assert mmst.apples == 0
     assert mmst.oranges == 0
+    assert mmst.zebras == 0
 
     mmst.apples = 1
     assert mmst.apples == 1
     assert mmst.oranges == 0
+    assert mmst.zebras == 0
+
+    mmst.zebras = 9001
+    assert mmst.apples == 1
+    assert mmst.oranges == 0
+    assert mmst.zebras == 9001
