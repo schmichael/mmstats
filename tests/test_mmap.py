30f1156140a4a246a2090aa3e8d5183ceea0beed
Michael Schurter <m@schmichael.com>
2011-09-21 00:10:20 -0500

    Add some more mmap related tests

--- tests/test_mmap.py
+++ tests/test_mmap.py
@@ -19,3 +19,35 @@ class TestMmap(base.MmstatsTestCase):
                 path=self.path, filename='mmstats-test_init_alt_name')
         self.assertEqual(fn, expected_fn)
         self.assertTrue(os.path.exists(fn))
+
+    def test_size_adjusting1(self):
+        """mmapped files must be at least PAGESIZE in size"""
+        _, sz, m = mmstats._init_mmap(path=self.path,
+                filename='mmstats-test_size_adjusting-1', size=1)
+
+        self.assertEqual(sz, mmstats.PAGESIZE)
+        self.assertEqual(m[:], '\x00' * mmstats.PAGESIZE)
+
+    def test_size_adjusting2(self):
+        """mmapped files must be multiples of PAGESIZE"""
+        _, sz, m = mmstats._init_mmap(
+                path=self.path,
+                filename='mmstats-test_size_adjusting-2',
+                size=(mmstats.PAGESIZE+1)
+            )
+
+        self.assertEqual(sz, mmstats.PAGESIZE * 2)
+        self.assertEqual(m[:], '\x00' * mmstats.PAGESIZE * 2)
+
+    def test_truncate(self):
+        """mmapped files must be initialized with null bytes"""
+        fn, sz, m = mmstats._init_mmap(
+                path=self.path,
+                filename='mmstats-test_truncate',
+            )
+
+        m[0] = 'X'
+
+        reopened_file = open(fn)
+        self.assertEqual(reopened_file.read(1), 'X')
+        self.assertEqual(reopened_file.read(1), '\x00')
