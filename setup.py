268f48b8a709236f9735f2f103550d2b3b92c725
Michael Schurter <m@schmichael.com>
2011-10-04 18:52:35 -0700

    mmstats "Origins" 0.3.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 setup(
     name='mmstats',
     url='https://github.com/schmichael/mmstats',
-    version='0.3.3',
+    version='0.3.4',
     license='BSD',
     author='Michael Schurter',
     author_email='m@schmichael.com',
