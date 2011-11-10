1301e82d6c41a96bc8a51d86081a76e1fc44fc4d
Michael Schurter <m@schmichael.com>
2011-11-09 18:04:38 -0800

    mmstats "The M is for Mongo" 0.3.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 setup(
     name='mmstats',
     url='https://github.com/schmichael/mmstats',
-    version='0.3.5',
+    version='0.3.6',
     license='BSD',
     author='Michael Schurter',
     author_email='m@schmichael.com',
