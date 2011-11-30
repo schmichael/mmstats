dab8a6bcf78edc58fb636b2115bb6d86dbe5ee02
Michael Schurter <m@schmichael.com>
2011-11-29 16:46:34 -0800

    mmstats "Rawr" 0.3.11

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 setup(
     name='mmstats',
     url='https://github.com/schmichael/mmstats',
-    version='0.3.10',
+    version='0.3.11',
     license='BSD',
     author='Michael Schurter',
     author_email='m@schmichael.com',
