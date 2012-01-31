d89bf849e802ce325e0d21690bbecfd7909dc968
Michael Schurter <m@schmichael.com>
2012-01-31 13:45:35 -0800

    mmstats "Derpstats" 0.4.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 setup(
     name='mmstats',
     url='https://github.com/schmichael/mmstats',
-    version='0.4.0',
+    version='0.4.1',
     license='BSD',
     author='Michael Schurter',
     author_email='m@schmichael.com',
