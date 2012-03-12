ffc3b805fc5ea7aea45cc88052aea24c1bd439d8
Michael Schurter <m@schmichael.com>
2012-03-12 12:00:06 -0700

    Starting to hack in Windows support

--- mmstats/_mmap.py
+++ mmstats/_mmap.py
@@ -4,6 +4,7 @@ import errno
 libc = ctypes.cdll.LoadLibrary(ctypes.util.find_library('c'))
 import mmap as stdlib_mmap
 import os
+import sys
 
 
 from . import libgettid
@@ -39,35 +40,46 @@ def init_mmap(path=DEFAULT_PATH, filename=DEFAULT_FILENAME, size=PAGESIZE):
     return (fd, full_path, size, m_ptr)
 
 
-# Linux consts from /usr/include/bits/mman.h
-MS_ASYNC = 1
-MS_SYNC = 4
+if sys.platform == 'win32':
+    """
+    LPVOID WINAPI MapViewOfFile(
+        __in  HANDLE hFileMappingObject,
+        __in  DWORD dwDesiredAccess,
+        __in  DWORD dwFileOffsetHigh,
+        __in  DWORD dwFileOffsetLow,
+        __in  SIZE_T dwNumberOfBytesToMap
+    );
+    """
+else:
+    # Linux consts from /usr/include/bits/mman.h
+    MS_ASYNC = 1
+    MS_SYNC = 4
 
 
-libc.mmap.restype = ctypes.c_void_p
-libc.mmap.argtypes = [
-    ctypes.c_void_p, # address
-    ctypes.c_size_t, # size of mapping
-    ctypes.c_int,    # protection
-    ctypes.c_int,    # flags
-    ctypes.c_int,    # fd
-    ctypes.c_int,    # offset (needs to be off_t type?)
-]
+    libc.mmap.restype = ctypes.c_void_p
+    libc.mmap.argtypes = [
+        ctypes.c_void_p, # address
+        ctypes.c_size_t, # size of mapping
+        ctypes.c_int,    # protection
+        ctypes.c_int,    # flags
+        ctypes.c_int,    # fd
+        ctypes.c_int,    # offset (needs to be off_t type?)
+    ]
 
 
-def mmap(size, fd):
-    m_ptr = libc.mmap(None,
-                      size,
-                      stdlib_mmap.PROT_READ | stdlib_mmap.PROT_WRITE,
-                      stdlib_mmap.MAP_SHARED,
-                      fd,
-                      0
-            )
-    if m_ptr == -1:
-        # Error
-        e = ctypes.get_errno()
-        raise OSError(e, errno.errorcode[e])
-    return m_ptr
+    def mmap(size, fd):
+        m_ptr = libc.mmap(None,
+                          size,
+                          stdlib_mmap.PROT_READ | stdlib_mmap.PROT_WRITE,
+                          stdlib_mmap.MAP_SHARED,
+                          fd,
+                          0
+                )
+        if m_ptr == -1:
+            # Error
+            e = ctypes.get_errno()
+            raise OSError(e, errno.errorcode[e])
+        return m_ptr
 
 
 libc.msync.restype = ctypes.c_int
