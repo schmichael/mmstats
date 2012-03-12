5f50ecdcb111bda88d1fe81dd11d87b2480208e4
Michael Schurter <m@schmichael.com>
2012-03-12 14:23:39 -0700

    Initial working attempt at dynamically adding fields to mmstats

--- mmstats/models.py
+++ mmstats/models.py
@@ -7,6 +7,14 @@ from . import fields, libgettid, _mmap
 from .defaults import DEFAULT_PATH, DEFAULT_FILENAME
 
 
+class OutOfSpaceError(Exception):
+    """No more room in mmap for extra fields"""
+
+
+class NameCollisionError(Exception):
+    """Attribute already exists with field name specified"""
+
+
 class FieldState(object):
     """Holds field state for each Field instance"""
 
@@ -18,9 +26,17 @@ class BaseMmStats(object):
     """Stats models should inherit from this"""
 
     def __init__(self, path=DEFAULT_PATH, filename=DEFAULT_FILENAME,
-            label_prefix=None):
+            label_prefix=None, extra_space=0):
         """\
-        Optionally given a filename or label_prefix, create an MmStats instance
+        Create an MmStats instance (instances are *not* threadsafe)
+
+        Optionally give a ``filename`` and/or ``path`` to control where the
+        mmap files are created.
+
+        ``label_prefix`` will be prepended to all field names.
+
+        ``extra_space`` is the amount of extra space in bytes to leave at the
+        end of the mmap region. Specify this if you dynamically add fields.
         """
         self._removed = False
 
@@ -40,6 +56,10 @@ class BaseMmStats(object):
                         and isinstance(attrval, fields.Field)):
                     total_size += self._add_field(attrname, attrval)
 
+        # Add in free_space before creating mmap
+        total_size += extra_space
+
+        # Create mmap
         self._fd, self._filename, self._size, self._mm_ptr = _mmap.init_mmap(
             path=path, filename=filename, size=total_size)
         mmap_t = ctypes.c_char * self._size
@@ -47,23 +67,48 @@ class BaseMmStats(object):
         ver = ctypes.c_byte.from_address(self._mm_ptr)
         ver.value = 1  # Version number
 
-        # Finally initialize thes stats
-        self._init_fields(total_size)
+        # Finally initialize the stats
+        self._init_fields()
+
+    def add_field(self, name, field):
+        """Add a ``field`` with the given ``name`` to an MmStats instance
+
+        Raises ``OutOfSpaceError`` exception if you attempt to add a field and
+        there's no space left in the mmap. Set a large extra_space kwarg to
+        fix.
+        """
+        if hasattr(self, name):
+            raise NameCollisionError(
+                    "Instance already has attribute named `%s`" % name)
+        size = self._add_field(name, field)
+        if self._offset + size > self.size:
+            raise OutOfSpaceError(
+                    "Cannot create field `%s` - requires %d bytes but only %d "
+                    "free. Raise extra_space from %d to at least %d" % (
+                        name, size, self.size - self._offset, self.extra_space,
+                        self.extra_space + size)
+                )
+        # _add_field(...) populated self._fields[name] with a state instance
+        self._init_field(self._fields[name])
+
+        # Add the field as an attribute to the class to support descriptors
+        setattr(self.__class__, name, field)
 
     def _add_field(self, name, field):
-        """Given a name and Field instance, add this field and retun size"""
+        """Given a name and Field instance, add this field and return size"""
         # Stats need a place to store their per Mmstats instance state
         state = self._fields[name] = FieldState(field)
 
         # Call field._new to determine size
         return field._new(state, self.label_prefix, name)
 
-    def _init_fields(self, total_size):
-        """Once all fields have been added, initialize them in mmap"""
+    def _init_field(self, state):
+        """Given a field's ``state`` object,  call it's _init(...) method"""
+        self._offset = state.field._init(state, self._mm_ptr, self._offset)
 
-        for state in self._fields.values():
-            # 2nd Call field._init to initialize new stat
-            self._offset = state.field._init(state, self._mm_ptr, self._offset)
+    def _init_fields(self):
+        """Once all fields have been added, initialize them in mmap"""
+        map(self._init_field, self._fields.values())
 
     @property
     def filename(self):
